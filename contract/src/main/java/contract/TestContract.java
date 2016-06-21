0cc0642b0ddf95de5450fa097cc24e39f6fb003d
SeenZe <maxime.santens@viacesi.fr>
2016-06-21 11:07:21 +0200

    MS - JUnit

---
+++ contract/src/main/java/contract/TestContract.java
@@ -0,0 +1,14 @@
+package contract;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestContract {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
